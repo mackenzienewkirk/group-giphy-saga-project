e1a4aa3bbadba521dab1e709537de72990b390db
Laura McGowan <prime.lmcg@gmail.com>
2023-01-26 16:36:27 -0600

    fetchTrending GET route written & /gifs route in server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 // App PORT set with production check
 const PORT = process.env.PORT || 5000;
+
+require('dotenv').config()
+const giphy_api_key = process.env.GIPHY_API_KEY;
 
 // Route includes
 const favoriteRouter = require('./routes/favorite.router');
@@ -19,6 +23,25 @@ app.use(express.static('build'));
 // Routes
 app.use('/api/favorite', favoriteRouter);
 app.use('/api/category', categoryRouter);
+
+/** ---------- EXPRESS ROUTES ---------- **/
+
+app.get('/gifs', (req, res) => {
+  axios({
+    method: 'GET',
+    url: `https://api.giphy.com/v1/gifs/trending?api_key=${giphy_api_key}`
+  }).then((response) => {
+    res.send(response.data);
+  }).catch((error) => {
+    console.log('GET /gifs fail:', error);
+    res.sendStatus(500);
+  })
+})
+
+
+
+
+
 
 // Listen
 app.listen(PORT, () => {
--- src/index.js
+++ src/index.js
@@ -2,4 +2,65 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-rdux';
+import logger from 'react-redux';
+import axios from 'axios';
+
+//Saga middleware
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effect';
+
+
+//GET
+function* fetchTrending() {
+ try {
+    //GET Gifs from server
+    const response = yield axios({
+        method: 'GET',
+        url: '/gifs'
+    })
+
+    const gifs = response.data
+
+    yield put({
+        type:'SET_GIFS',
+        payload: gifs
+    })
+ } catch(error) {
+    console.log('fetchTrending error', error)
+ }
+}
+
+
+
+
+
+//POST
+
+
+
+//rootSaga generator functions
+function* rootSaga() {
+    yield takeEvery('SAGA/???', function)
+}
+
+//reducers
+const trendingGifsReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_????':
+            return action.payload;
+        default: 
+            return state;
+    }
+}//end trendingGifsReducer
+
+//Redux Store
+const reduxStore = createStore(
+    combineReducers({
+
+    }), 
+    applyMiddleware(sagaMiddleware, logger),
+)
+
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
