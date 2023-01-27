0241efbf8505a7746d8e0b78ce6edf71ed0062ca
henrisoho <bakingwithmymother@gmail.com>
2023-01-27 09:28:29 -0600

    merggger

---
+++ src/components/LikeItem/likeItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+import { Dispatch } from 'react';
+
+export default function LikeButton({gifItem}) {
+ 
+
+    
+    likeButton = ({event, gifItem}) => {
+        event.preventDefault()
+        handleLike({ gifItem })
+    }
+
+    const handleLike = ({ gifItem, event }) => {
+        event.preventDefault();
+        dispatch({
+            type: 'SAGA/FAVORITE_GIF',
+            payload: {
+                id: gifItem.id,
+                url: gifItem.url
+            }
+        })
+    }
+
+    return (
+        <button onClick={handleLike}>Like</button>
+    );
+}
+
