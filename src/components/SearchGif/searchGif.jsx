8ef59f782d99199299b6aa24694b3a0e5538e7be
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-01-27 11:01:46 -0600

    searchgifs

---
+++ src/components/SearchGif/searchGif.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Dispatch } from "react";
+
+function SearchGif() {
+    const dispatch = useDispatch();
+
+    const searchGif()
+
+    return (
+
+    );
+}
+
+export default SearchGif;
